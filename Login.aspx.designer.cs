9af4acf9236febe99a9ae9ab8f51dbf6ef782e90
Xavier Hughes <xwjhughes@gmail.com>
2024-05-14 16:59:09 -0600

    Okay

--- Login.aspx.designer.cs
+++ Login.aspx.designer.cs
@@ -78,15 +78,6 @@ namespace PMS
         protected global::System.Web.UI.WebControls.LinkButton lbnRegister;
 
         /// <summary>
-        /// panelRegister control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Panel panelRegister;
-
-        /// <summary>
         /// txtRegisterUserID control.
         /// </summary>
         /// <remarks>
