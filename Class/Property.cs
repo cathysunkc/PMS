eb7851fbb56a9077f7cf54d92da3bc6849729ddd
Harryuen <kifung.yuen@edu.sait.ca>
2024-05-22 21:30:39 -0600

    testing

--- Class/Property.cs
+++ Class/Property.cs
@@ -95,135 +95,138 @@ namespace PMS
             return this.Address + "\n" + this.City + "\n" + this.ZipCode;        
         }
 
-		public Property GetPropertyByID(string propertyID)
-        {
-            
-            DataTable dt = (TempPropertyRecords()).Select($"property_id = '{propertyID}'").CopyToDataTable();
+        //Records for prototype
+/*
+public Property GetPropertyByID(string propertyID)
+{
 
-            DataRow dr = dt.Rows[0];
-            Property property = new Property(propertyID);
-            property.PropertyID = propertyID;
-            property.Address = dr["address"].ToString();
-            property.ZipCode = dr["zip_code"].ToString();
-            property.City = dr["city"].ToString();
-            property.PropertyType = dr["property_type"].ToString();
-            property.BedNum = Convert.ToDouble(dr["bed_num"]);
-            property.BathNum = Convert.ToDouble(dr["bath_num"]);
-            property.Area = Convert.ToDouble(dr["area"]);
-            property.ParkingType = dr["parking_type"].ToString();
-            property.PostedDate = Convert.ToDateTime(dr["posted_date"]);
-            property.AvailableDate = Convert.ToDateTime(dr["available_date"]);
-            property.Description = dr["description"].ToString();
-            property.IsFeatured = Convert.ToBoolean(dr["is_featured"]);
-            property.TransactionType = Convert.ToChar(dr["transaction_type"]);
-            property.Price = Convert.ToDouble(dr["price"]);
-            property.ImagePath = dr["image_path"].ToString();
-            property.RealtorID = dr["realtor_id"].ToString();
-            property.IsSold = Convert.ToBoolean(dr["is_sold"]);
+    DataTable dt = (TempPropertyRecords()).Select($"property_id = '{propertyID}'").CopyToDataTable();
 
-            return property;
-        }
+    DataRow dr = dt.Rows[0];
+    Property property = new Property(propertyID);
+    property.PropertyID = propertyID;
+    property.Address = dr["address"].ToString();
+    property.ZipCode = dr["zip_code"].ToString();
+    property.City = dr["city"].ToString();
+    property.PropertyType = dr["property_type"].ToString();
+    property.BedNum = Convert.ToDouble(dr["bed_num"]);
+    property.BathNum = Convert.ToDouble(dr["bath_num"]);
+    property.Area = Convert.ToDouble(dr["area"]);
+    property.ParkingType = dr["parking_type"].ToString();
+    property.PostedDate = Convert.ToDateTime(dr["posted_date"]);
+    property.AvailableDate = Convert.ToDateTime(dr["available_date"]);
+    property.Description = dr["description"].ToString();
+    property.IsFeatured = Convert.ToBoolean(dr["is_featured"]);
+    property.TransactionType = Convert.ToChar(dr["transaction_type"]);
+    property.Price = Convert.ToDouble(dr["price"]);
+    property.ImagePath = dr["image_path"].ToString();
+    property.RealtorID = dr["realtor_id"].ToString();
+    property.IsSold = Convert.ToBoolean(dr["is_sold"]);
 
-        public DataTable GetFeaturedProperty()
-        {
-            DataTable dt = (TempPropertyRecords()).Select("is_featured = true AND is_sold = false").CopyToDataTable();
+    return property;
+}
 
-            return dt;
-        }
+public DataTable GetFeaturedProperty()
+{
+    DataTable dt = (TempPropertyRecords()).Select("is_featured = true AND is_sold = false").CopyToDataTable();
+
+    return dt;
+}
 
 
-        public DataTable FindProperty(char transactionType, double bedNum, double bathNum)
-        {
-            DataTable dt = (TempPropertyRecords()).Select("is_sold = false").CopyToDataTable();
+public DataTable FindProperty(char transactionType, double bedNum, double bathNum)
+{
+    DataTable dt = (TempPropertyRecords()).Select("is_sold = false").CopyToDataTable();
 
-            string query = string.Empty;
+    string query = string.Empty;
 
-            if (transactionType != ' ')
-            {
-                query += $"transaction_type = '{transactionType}'";               
-            }
-
-            if (bedNum != 0)
-            {
-                if (query != string.Empty)
-                    query += $" AND bed_num = {bedNum}";
-                else
-                    query += $" bed_num = {bedNum}";
-
-            }
-
-            if (bathNum != 0)
-            {
-                if (query != string.Empty)
-                    query += $" AND bath_num = {bathNum}";
-                else
-                    query += $" bath_num = {bathNum}";
-                                       
-            }
-
-            DataRow[] dr = (dt).Select(query);
-
-            if (dr.Length > 0)
-                dt = dr.CopyToDataTable();
-            else if (dr.Length == 0)
-                dt = new DataTable();
-
-            return dt;
-        }
-
-        
-        //Records for Prototype
-        public DataTable TempPropertyRecords()
-        {
-            DataTable dt = new DataTable("Properties");
-
-            dt.Columns.Add("property_id", typeof(string));
-            dt.Columns.Add("address", typeof(string));
-            dt.Columns.Add("zip_code", typeof(string));
-            dt.Columns.Add("city", typeof(string));
-            dt.Columns.Add("property_type", typeof(string));
-            dt.Columns.Add("bed_num", typeof(double));
-            dt.Columns.Add("bath_num", typeof(double));
-            dt.Columns.Add("area", typeof(double));
-            dt.Columns.Add("parking_type", typeof(string));
-            dt.Columns.Add("posted_date", typeof(DateTime));
-            dt.Columns.Add("available_date", typeof(DateTime));
-            dt.Columns.Add("description", typeof(string));
-            dt.Columns.Add("is_featured", typeof(bool));
-            dt.Columns.Add("transaction_type", typeof(char));
-            dt.Columns.Add("price", typeof(double));
-            dt.Columns.Add("image_path", typeof(string));
-            dt.Columns.Add("realtor_id", typeof(string));
-            dt.Columns.Add("is_sold", typeof(bool));
-
-            dt.PrimaryKey = new DataColumn[] { dt.Columns["property_id"] };
-
-            dt.Rows.Add("P000001", "7038 34 Avenue NW", "T3B6E8", "Calgary, AB",
-                "Single Family", 2, 3, 1386, "Attached Garage (1)",
-                "2024-04-01", "2024-05-01", "Welcome to Arrive at Bowness, where innovation meets elegance! This townhouse, honoured with the esteemed 2017 Mayors Urban Design Award for Housing Innovation, is a beacon of contemporary living.",
-                true, 'S', 449900, "https://robbreport.com/wp-content/uploads/2022/08/The-Grand-by-Gehry_Great-Room_Image-Courtesy-of-Peter-Christiansen-Valli-for-The-Grand-by-Gehry.jpg", "realtor01", false);
-            dt.Rows.Add("P000002", "7224 Bow Crescent NW", "T3B2B9", "Calgary, AB",
-                "Vacant Land", 3, 3, 32000, "Underground",
-                "2024-04-03", "2024-05-05", "LOCATED IN THE HIGHLY SOUGHT AFTER COMMUNITY OF LAKE MAHOGANY. 2 BED PLUS DEN, 2 BATH, TITLED UNDERGROUND PARKING, AND STORAGE LOCKER!",
-                true, 'S', 379900, "https://www.idesignarch.com/wp-content/uploads/2686-Eagleridge-Drive-Coquitlam_1.jpg", "realtor01", false);
-            dt.Rows.Add("P000003", "56 Martingrove Way NE", "T3J2T2", "Calgary, AB",
-                "Single Family", 4, 2, 1605, "Attached Garage (2)",
-                "2024-04-02", "2024-05-10", "Nestled within the vibrant community of Copperfield, this perfectly upgraded END UNIT townhome invites you to indulge in a lifestyle of convenience and style.",
-                false, 'S', 450000, "https://www.homestratosphere.com/wp-content/uploads/2020/03/1-country-home-shiplap-walls-march232020-min.jpg", "realtor01", false);
-            dt.Rows.Add("P000004", "15 Templegreen Road NE", "T1Y4Y9", "Calgary, AB",
-                "Single Family", 4, 4, 1317, "Detached Garage (2)",
-                "2024-04-02", "2024-05-30", "3 Bed 1.5 Bath House in Temple with Double Car Garage - More Photos Coming Soon!Located in the established community of Temple, this updated 1100+ square foot detached house with a double car garage is the perfect home with a variety of features. ",
-                false, 'R', 2200, "https://www.real-samui-properties.com/prodimages/Villa-Ayundra---February-2015-121.jpg", "realtor01", false);
-            dt.Rows.Add("P000005", "2356 Northmount Drive NW", "T2L0C1", "Calgary, AB",
-               "Single Family", 2, 1, 2500, "Parking Pad",
-               "2024-04-02", "2024-05-30", "Legal Suite BASEMENT with 2 bedrooms 1 bathroom - Tenants will have their separate entrance, kitchen, washer and dryer, microwave and parking space.",
-               false, 'R', 1000, "https://hgtvhome.sndimg.com/content/dam/images/hgtv/fullset/2016/7/21/0/FOD16_Decor-Aid_Urban_9.jpg.rend.hgtvcom.616.493.suffix/1469125819106.jpeg", "realtor01", false);
-            dt.Rows.Add("P000006", "50 Hamptons Manor NW", "T3A6K2", "Calgary, AB",
-               "Single Family", 2, 1, 3400, "Attached Garage (3)",
-               "2024-04-03", "2024-05-15", "Introducing a Spectacular Residence in Hamptons for Rent:Property Size: 3400 SQ FT **Breathtaking Views**: Enjoy stunning vistas of the golf course pond, adding a serene and tranquil backdrop to your daily life.",
-               false, 'R', 4995, "https://cdn.britannica.com/05/157305-050-FD9CB47C.jpg", "realtor01", false);
-
-            return dt;
-        }
+    if (transactionType != ' ')
+    {
+        query += $"transaction_type = '{transactionType}'";               
     }
+
+    if (bedNum != 0)
+    {
+        if (query != string.Empty)
+            query += $" AND bed_num = {bedNum}";
+        else
+            query += $" bed_num = {bedNum}";
+
+    }
+
+    if (bathNum != 0)
+    {
+        if (query != string.Empty)
+            query += $" AND bath_num = {bathNum}";
+        else
+            query += $" bath_num = {bathNum}";
+
+    }
+
+    DataRow[] dr = (dt).Select(query);
+
+    if (dr.Length > 0)
+        dt = dr.CopyToDataTable();
+    else if (dr.Length == 0)
+        dt = new DataTable();
+
+    return dt;
+}
+
+
+//Records for Prototype
+public DataTable TempPropertyRecords()
+{
+    DataTable dt = new DataTable("Properties");
+
+    dt.Columns.Add("property_id", typeof(string));
+    dt.Columns.Add("address", typeof(string));
+    dt.Columns.Add("zip_code", typeof(string));
+    dt.Columns.Add("city", typeof(string));
+    dt.Columns.Add("property_type", typeof(string));
+    dt.Columns.Add("bed_num", typeof(double));
+    dt.Columns.Add("bath_num", typeof(double));
+    dt.Columns.Add("area", typeof(double));
+    dt.Columns.Add("parking_type", typeof(string));
+    dt.Columns.Add("posted_date", typeof(DateTime));
+    dt.Columns.Add("available_date", typeof(DateTime));
+    dt.Columns.Add("description", typeof(string));
+    dt.Columns.Add("is_featured", typeof(bool));
+    dt.Columns.Add("transaction_type", typeof(char));
+    dt.Columns.Add("price", typeof(double));
+    dt.Columns.Add("image_path", typeof(string));
+    dt.Columns.Add("realtor_id", typeof(string));
+    dt.Columns.Add("is_sold", typeof(bool));
+
+    dt.PrimaryKey = new DataColumn[] { dt.Columns["property_id"] };
+
+    dt.Rows.Add("P000001", "7038 34 Avenue NW", "T3B6E8", "Calgary, AB",
+        "Single Family", 2, 3, 1386, "Attached Garage (1)",
+        "2024-04-01", "2024-05-01", "Welcome to Arrive at Bowness, where innovation meets elegance! This townhouse, honoured with the esteemed 2017 Mayors Urban Design Award for Housing Innovation, is a beacon of contemporary living.",
+        true, 'S', 449900, "https://robbreport.com/wp-content/uploads/2022/08/The-Grand-by-Gehry_Great-Room_Image-Courtesy-of-Peter-Christiansen-Valli-for-The-Grand-by-Gehry.jpg", "realtor01", false);
+    dt.Rows.Add("P000002", "7224 Bow Crescent NW", "T3B2B9", "Calgary, AB",
+        "Vacant Land", 3, 3, 32000, "Underground",
+        "2024-04-03", "2024-05-05", "LOCATED IN THE HIGHLY SOUGHT AFTER COMMUNITY OF LAKE MAHOGANY. 2 BED PLUS DEN, 2 BATH, TITLED UNDERGROUND PARKING, AND STORAGE LOCKER!",
+        true, 'S', 379900, "https://www.idesignarch.com/wp-content/uploads/2686-Eagleridge-Drive-Coquitlam_1.jpg", "realtor01", false);
+    dt.Rows.Add("P000003", "56 Martingrove Way NE", "T3J2T2", "Calgary, AB",
+        "Single Family", 4, 2, 1605, "Attached Garage (2)",
+        "2024-04-02", "2024-05-10", "Nestled within the vibrant community of Copperfield, this perfectly upgraded END UNIT townhome invites you to indulge in a lifestyle of convenience and style.",
+        false, 'S', 450000, "https://www.homestratosphere.com/wp-content/uploads/2020/03/1-country-home-shiplap-walls-march232020-min.jpg", "realtor01", false);
+    dt.Rows.Add("P000004", "15 Templegreen Road NE", "T1Y4Y9", "Calgary, AB",
+        "Single Family", 4, 4, 1317, "Detached Garage (2)",
+        "2024-04-02", "2024-05-30", "3 Bed 1.5 Bath House in Temple with Double Car Garage - More Photos Coming Soon!Located in the established community of Temple, this updated 1100+ square foot detached house with a double car garage is the perfect home with a variety of features. ",
+        false, 'R', 2200, "https://www.real-samui-properties.com/prodimages/Villa-Ayundra---February-2015-121.jpg", "realtor01", false);
+    dt.Rows.Add("P000005", "2356 Northmount Drive NW", "T2L0C1", "Calgary, AB",
+       "Single Family", 2, 1, 2500, "Parking Pad",
+       "2024-04-02", "2024-05-30", "Legal Suite BASEMENT with 2 bedrooms 1 bathroom - Tenants will have their separate entrance, kitchen, washer and dryer, microwave and parking space.",
+       false, 'R', 1000, "https://hgtvhome.sndimg.com/content/dam/images/hgtv/fullset/2016/7/21/0/FOD16_Decor-Aid_Urban_9.jpg.rend.hgtvcom.616.493.suffix/1469125819106.jpeg", "realtor01", false);
+    dt.Rows.Add("P000006", "50 Hamptons Manor NW", "T3A6K2", "Calgary, AB",
+       "Single Family", 2, 1, 3400, "Attached Garage (3)",
+       "2024-04-03", "2024-05-15", "Introducing a Spectacular Residence in Hamptons for Rent:Property Size: 3400 SQ FT **Breathtaking Views**: Enjoy stunning vistas of the golf course pond, adding a serene and tranquil backdrop to your daily life.",
+       false, 'R', 4995, "https://cdn.britannica.com/05/157305-050-FD9CB47C.jpg", "realtor01", false);
+
+    return dt;
+}
+}
+*/
 }
